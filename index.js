5efbe706d881022cb3f927292c948deb8736deb3
SiddiquiArfat <arfatsiddiqui070@gmail.com>
2023-02-25 22:02:41 +0530

    fw24_716 Day5

--- index.js
+++ index.js
@@ -40,11 +40,15 @@ window.addEventListener("load",()=>{
       data.forEach((el,i)=>{
         let card = document.createElement("div")
         card.setAttribute("class","card")
+        
 
         let image = document.createElement("img")
         image.setAttribute("src",el.image)
         image.setAttribute("id","image")
-
+        image.addEventListener('click',()=>{
+          fundisp(el.id);
+          document.getElementById('sidebar').classList.toggle('active');
+        })
         let price = document.createElement("p")
         price.textContent = el.price
 
@@ -55,13 +59,25 @@ window.addEventListener("load",()=>{
           description.textContent = el.description
           description.setAttribute("id","desc")
 
-        
-
-        
-
-
         card.append(image,price,name,description)
         container.append(card)
 
       })
+    }
+
+    let fundisp = async (id)=>{
+      let a = await fetch(`${url}/${id}`);
+      let pro = await a.json();
+      append(pro);
+    }
+
+    let append=(pro)=>{
+      let side = document.getElementById('sidebar');
+      let cardd=`<img src="${pro.image}" class="sideimage">
+        <div class = "headings">
+        <p class="sideprice">₹ ${pro.price}</p>
+        <h2>${pro.name}</h2>
+        <p>${pro.description}</p>
+        </div>`;
+      side.innerHTML = cardd;
     }
