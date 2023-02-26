861879323235de16b7481a6173f7344fd8fe8fd5
unknown <bhanuupratapp@gmail.com>
2023-02-27 00:56:44 +0530

    done

--- index.js
+++ index.js
@@ -1,81 +1,99 @@
-const url = "https://striped-telling-dryer.glitch.me/pizza";
-const non_veg =
-  "https://striped-telling-dryer.glitch.me/pizza?category=Non-Veg";
-const vege = "https://striped-telling-dryer.glitch.me/pizza?category=Veg";
 
-let container = document.querySelector("#container");
-let cartContainer = document.querySelector("#cart-container");
-let priceContainer = document.querySelector("#price-container");
-var orderData = JSON.parse(localStorage.getItem("cart")) || [];
+const url = "https://striped-telling-dryer.glitch.me/pizza"
+const non_veg = "https://striped-telling-dryer.glitch.me/pizza?category=Non-Veg"
+const vege = "https://striped-telling-dryer.glitch.me/pizza?category=Veg"
 
-let totalArr = [];
+let container = document.querySelector("#container")
+let cartContainer = document.querySelector("#cart-container")
+let priceContainer = document.querySelector("#price-container")
+var orderData = JSON.parse(localStorage.getItem("cart")) || []
 
-let Data = [];
 
-let arr2 = JSON.parse(localStorage.getItem("total")) || [];
-let bag1 = 0;
-for (let i = 0; i < arr2.length; i++) {
-  bag1 = bag1 + arr2[i];
-}
+let totalArr = []
 
-var totalPrice = bag1;
 
-displaydata3();
+let Data = []
+
+let arr2 = JSON.parse(localStorage.getItem("total"))||[]
+let bag1 = 0
+ for(let i=0;i<arr2.length;i++){
+ bag1 = bag1+arr2[i]
+ }
+
+ var totalPrice = bag1
+
+
+displaydata3()
+
+
+
+
 
 window.addEventListener("load", () => {
-  fetchData(url);
+  fetchData(url)
 
-  var bs = document.querySelector("#BS");
-  bs.style.color = "#6c9bb3";
-  bs.style.borderBottom = "2px solid #82bb37";
-
+  var bs = document.querySelector("#BS")
+  bs.style.color = "#6c9bb3"
+  bs.style.borderBottom = '2px solid #82bb37'
+  
   if (cartContainer.innerHTML == "") {
-    displaydata2();
+
+    displaydata2()
   }
 
-  let nonvpizza = document.querySelector("#non-veg");
-  nonvpizza.addEventListener("click", function () {
-    fetchData(non_veg);
-    nonvpizza.style.color = "#6c9bb3";
-    nonvpizza.style.borderBottom = "2px solid #82bb37";
 
-    vegpizza.style.color = "unset";
-    vegpizza.style.borderBottom = "none";
+  let nonvpizza = document.querySelector("#non-veg")
+  nonvpizza.addEventListener("click",function(){
+    fetchData(non_veg)
+    nonvpizza.style.color = "#6c9bb3"
+    nonvpizza.style.borderBottom = '2px solid #82bb37'
+   
+    vegpizza.style.color = 'unset'
+    vegpizza.style.borderBottom = "none"
 
-    bs.style.color = "unset";
-    bs.style.borderBottom = "none";
-  });
+    bs.style.color = "unset"
+  bs.style.borderBottom = 'none'
 
-  let vegpizza = document.querySelector("#veg");
-  vegpizza.addEventListener("click", function () {
-    fetchData(vege);
-    vegpizza.style.color = "#6c9bb3";
-    vegpizza.style.borderBottom = "2px solid #82bb37";
+    
+  })
 
-    nonvpizza.style.color = "unset";
-    nonvpizza.style.borderBottom = "none";
+  let vegpizza = document.querySelector("#veg")
+  vegpizza.addEventListener("click",function(){
+    fetchData(vege)
+    vegpizza.style.color = "#6c9bb3"
+    vegpizza.style.borderBottom = '2px solid #82bb37'
+   
+    nonvpizza.style.color = "unset"
+    nonvpizza.style.borderBottom = 'none'
 
-    bs.style.color = "unset";
-    bs.style.borderBottom = "none";
-  });
+    bs.style.color = "unset"
+  bs.style.borderBottom = 'none'
+    
+  })
 
-  bs.addEventListener("click", function () {
-    fetchData(url);
-    bs.style.color = "#6c9bb3";
-    bs.style.borderBottom = "2px solid #82bb37";
+  
+  bs.addEventListener("click",function(){
+  fetchData(url)
+  bs.style.color = "#6c9bb3"
+  bs.style.borderBottom = '2px solid #82bb37'
 
-    vegpizza.style.color = "unset";
-    vegpizza.style.borderBottom = "none";
+  vegpizza.style.color = "unset"
+  vegpizza.style.borderBottom = 'none'
+ 
+  nonvpizza.style.color = "unset"
+  nonvpizza.style.borderBottom = 'none'
 
-    nonvpizza.style.color = "unset";
-    nonvpizza.style.borderBottom = "none";
-  });
-});
+})
+
+  
+
+})
 
 function fetchData(params) {
   fetch(params)
     .then((res) => res.json())
     .then((aka) => {
+
       Data = aka.map((e, i) => {
         return {
           brand: e.brand,
@@ -83,51 +101,78 @@ function fetchData(params) {
           title: e.title,
           image: e.image,
           description: e.description,
+          size:e.size,
+          quantity:e.quantity,
           category: e.category,
-          size: e.size,
-          quantity: e.quantity,
           price: e.price,
-          id: e.id,
-          quantity: e.quantity,
-        };
-      });
-
-      displayData(Data);
-      console.log(aka);
+          id: e.id
+         
+        }
+      })
+      
+      displayData(Data)
+      console.log(aka)
     })
     .catch((error) => {
-      console.log(error);
-    });
+      console.log(error)
+    })
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function displayData(data) {
+
   container.innerHTML = null;
   data.forEach((el, i) => {
-    let card = document.createElement("div");
-    card.setAttribute("class", "card");
+    let card = document.createElement("div")
+    card.setAttribute("class", "card")
 
-    let image = document.createElement("img");
-    image.setAttribute("src", el.image);
-    image.setAttribute("id", "image");
+    let image = document.createElement("img")
+    image.setAttribute("src", el.image)
+    image.setAttribute("id", "image")
 
-    let price = document.createElement("p");
-    price.textContent = el.price;
-    price.setAttribute("class", "price");
+    let price = document.createElement("p")
+    price.textContent = el.price
+    price.setAttribute("class", "price")
 
-    let name = document.createElement("h2");
-    name.textContent = el.name;
-    name.setAttribute("id", "name");
 
-    let description = document.createElement("p");
-    description.textContent = el.description;
-    description.setAttribute("id", "desc");
+
+    let name = document.createElement("h2")
+    name.textContent = el.name
+    name.setAttribute("id", "name")
+
+    let description = document.createElement("p")
+    description.textContent = el.description
+    description.setAttribute("id", "desc")
+
+
+
 
     var array = ["Regular", "Medium", "Large"];
 
     //Create and append select list
     let selectList = document.createElement("select");
-    selectList.setAttribute("id", "selectList");
-    selectList.addEventListener("select", function () {});
+    selectList.setAttribute("id", "selectList")
+    selectList.addEventListener("select", function () {
+
+    })
+
+
 
     //Create and append the options
     for (var i = 0; i < array.length; i++) {
@@ -137,16 +182,14 @@ function displayData(data) {
       selectList.append(option);
     }
 
-    var array1 = [
-      "New Hand Tossed",
-      "100% Wheat Thin Crust",
-      "Cheese Burst",
-      "Fresh Pan Pizza",
-    ];
+
+    var array1 = ["New Hand Tossed", "100% Wheat Thin Crust", "Cheese Burst", "Fresh Pan Pizza"];
 
     //Create and append select list
     let List = document.createElement("select");
-    List.setAttribute("id", "List");
+    List.setAttribute("id", "List")
+
+
 
     //Create and append the options
     for (var i = 0; i < array1.length; i++) {
@@ -156,16 +199,25 @@ function displayData(data) {
       List.append(option);
     }
 
-    let btn = document.createElement("button");
-    btn.textContent = "ADD TO CART";
-    btn.setAttribute("id", "btn");
-
+    let btn = document.createElement("button")
+    btn.textContent = "ADD TO CART"
+    btn.setAttribute("id", "btn")
+    
     btn.addEventListener("click", function () {
-      let cartData = JSON.parse(localStorage.getItem("cart")) || [];
 
-      //*** */
+      
 
-      let isPresent = false;
+      let cartData = JSON.parse(localStorage.getItem("cart")) || []
+
+//*** */
+      
+
+    
+    
+    
+      
+
+let isPresent = false
       for (let i = 0; i < cartData.length; i++) {
         if (cartData[i].id == el.id) {
           isPresent = true;
@@ -174,82 +226,126 @@ function displayData(data) {
         }
       }
       if (isPresent == true) {
-        alert("Product already in the cart");
+        alert("Product already in the cart")
       } else {
-        cartData.push(el);
-        localStorage.setItem("cart", JSON.stringify(cartData));
+        cartData.push(el)
+        localStorage.setItem("cart", JSON.stringify(cartData))
         // alert("Product added to The cart")
-        let orderData = JSON.parse(localStorage.getItem("cart")) || [];
-        displaydata(orderData);
+        let orderData = JSON.parse(localStorage.getItem("cart")) || []
+        displaydata(orderData)
+     
+    //     let bag = 0
+    // cartData.forEach(function(ele){
+    //     bag=bag+Number(ele.price)
+    //     totalPrice=bag
 
-        //     let bag = 0
-        // cartData.forEach(function(ele){
-        //     bag=bag+Number(ele.price)
-        //     totalPrice=bag
-
-        // })
-
-        // document.querySelector("#hh22").innerText="Total price is: "+total
-        // console.log(totalPrice)
-
-        let totalArr = JSON.parse(localStorage.getItem("total")) || [];
-        totalArr.push(Number(el.price));
-        var bag = 0;
+      
+    // })
+    
+    // document.querySelector("#hh22").innerText="Total price is: "+total
+    // console.log(totalPrice)
+    
+        
+        let totalArr = JSON.parse(localStorage.getItem("total")) || []
+        totalArr.push(Number(el.price))
+        var bag = 0
         for (let i = 0; i < totalArr.length; i++) {
-          bag = bag + totalArr[i];
+          bag = bag + totalArr[i]
         }
-        console.log(totalArr);
-        totalPrice = bag;
-        localStorage.setItem("total", JSON.stringify(totalArr));
-
+console.log(totalArr)
+        totalPrice = bag
+        localStorage.setItem("total", JSON.stringify(totalArr))
+        
+        
         // let done = localStorage.setItem("total", JSON.stringify(totalArr))
 
         // length = orderData.length
 
         // console.log(length)
+       
+    
+       
 
-        displaydata3();
+      
+displaydata3()
+
+
+
+
+
+
       }
-    });
 
-    card.append(image, price, name, description, selectList, List, btn);
-    container.append(card);
-  });
+
+
+
+
+    })
+
+
+
+
+
+
+
+    card.append(image, price, name, description, selectList, List, btn)
+    container.append(card)
+
+  })
+
+  
 }
 
+
+
+
+
+
+
 function displaydata(data) {
-  cartContainer.innerHTML = null;
+
+
+  cartContainer.innerHTML = null
 
   data.forEach((el, i) => {
-    let cart = document.createElement("div");
+    let cart = document.createElement("div")
 
-    let img = document.createElement("img");
-    img.setAttribute("src", el.image);
-    img.setAttribute("id", "image");
+    let img = document.createElement("img")
+    img.setAttribute("src", el.image)
+    img.setAttribute("id", "image")
 
-    let prc = document.createElement("p");
-    prc.textContent = el.price;
-    prc.setAttribute("class", "price");
+    let prc = document.createElement("p")
+    prc.textContent = el.price
+    prc.setAttribute("class", "price")
 
-    let nam = document.createElement("h2");
-    nam.textContent = el.name;
-    nam.setAttribute("id", "name");
 
-    let desc = document.createElement("p");
-    desc.textContent = el.description;
-    desc.setAttribute("id", "desc");
 
-    let cplus = document.createElement("div");
-    cplus.setAttribute("id", "cplus");
+    let nam = document.createElement("h2")
+    nam.textContent = el.name
+    nam.setAttribute("id", "name")
 
-    let count = document.createElement("span");
-    count.textContent = Number(1);
+    let desc = document.createElement("p")
+    desc.textContent = el.description
+    desc.setAttribute("id", "desc")
 
-    let plus = document.createElement("button");
-    plus.textContent = "+";
+    
+    
 
-    plus.addEventListener("click", function () {
-      count.textContent = Number(count.textContent) + Number(1);
+
+
+   let cplus = document.createElement("div")
+   cplus.setAttribute("id","cplus")
+
+
+    let count = document.createElement("span")
+    count.textContent = Number(1)
+
+    let plus = document.createElement("button")
+    plus.textContent = "+"
+    
+    plus.addEventListener("click",function(){
+      count.textContent = Number(count.textContent) + Number(1)
+
 
       // var orderData = JSON.parse(localStorage.getItem("cart")) || []
       // totalPrice = totalPrice + Number(el.price)
@@ -258,14 +354,15 @@ function displaydata(data) {
       //   localStorage.setItem("cart", JSON.stringify(orderData))
       //   // displaydata(orderData)
       //   displaydata3()
-    });
 
-    let minus = document.createElement("button");
+    })
+    
+    let minus = document.createElement("button")
 
-    minus.textContent = "-";
-    minus.addEventListener("click", function () {
-      count.textContent -= Number(1);
-      var orderData = JSON.parse(localStorage.getItem("cart")) || [];
+      minus.textContent = "-"
+    minus.addEventListener("click",function(){
+      count.textContent -= Number(1)
+      var orderData = JSON.parse(localStorage.getItem("cart")) || []
       // totalPrice = totalPrice - Number(el.price)
       // console.log(totalPrice)
       // localStorage.setItem("total", JSON.stringify(totalPrice))
@@ -273,120 +370,128 @@ function displaydata(data) {
       //   // displaydata(orderData)
       //   displaydata3()
 
-      if (count.textContent < 1) {
-        orderData.splice(i, 1);
-        totalPrice = totalPrice - el.price;
-        localStorage.setItem("total", JSON.stringify(totalPrice));
-        localStorage.setItem("cart", JSON.stringify(orderData));
-        displaydata(orderData);
+
+      
+      if(count.textContent<1){
+        
+        orderData.splice(i,1)
+        totalPrice = totalPrice - el.price
+        localStorage.setItem("total", JSON.stringify(totalPrice))
+        localStorage.setItem("cart", JSON.stringify(orderData))
+        displaydata(orderData)
         if (cartContainer.innerHTML == "") {
-          displaydata2();
+
+          displaydata2()
         }
-
-        console.log("done");
+        
+        console.log('done')
       }
-      displaydata3();
-    });
+      displaydata3()
 
-    cplus.append(minus, count, plus);
-    cart.append(img, prc, nam, desc, cplus);
-    cartContainer.append(cart);
-  });
+    })
+    
+
+
+
+
+    cplus.append(minus,count,plus)
+    cart.append(img, prc, nam, desc,cplus)
+    cartContainer.append(cart)
+
+
+
+
+
+  })
 }
 
-displaydata(orderData);
+displaydata(orderData)
 
 function displaydata2() {
-  let cartImg = document.createElement("img");
 
-  cartImg.setAttribute(
-    "src",
-    "https://png.pngtree.com/png-vector/20220629/ourmid/pngtree-empty-shopping-cart-store-icon-png-image_5624129.png"
-  );
-  cartImg.setAttribute("class", "cartImg");
+  let cartImg = document.createElement("img")
 
-  let empty = document.createElement("h2");
-  empty.textContent = "YOUR CART IS EMPTY";
-  empty.setAttribute("class", "empty");
+  cartImg.setAttribute("src", "https://png.pngtree.com/png-vector/20220629/ourmid/pngtree-empty-shopping-cart-store-icon-png-image_5624129.png")
+  cartImg.setAttribute("class", "cartImg")
 
-  let addCart = document.createElement("p");
-  addCart.textContent = "Please add some items from the menu.";
-  addCart.setAttribute("class", "addCart");
+  let empty = document.createElement("h2")
+  empty.textContent = "YOUR CART IS EMPTY"
+  empty.setAttribute("class", "empty")
 
-  cartContainer.append(cartImg, empty, addCart);
+  let addCart = document.createElement("p")
+  addCart.textContent = "Please add some items from the menu."
+  addCart.setAttribute("class", "addCart")
+
+  cartContainer.append(cartImg, empty, addCart)
 }
 
-function displaydata3() {
 
+function displaydata3() {
   
   priceContainer.innerHTML = null
   let total = document.createElement("div")
   let totalP = document.createElement("h2")
   totalP.textContent = `Subtotal‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ${totalPrice}.00`
   totalP.setAttribute("class", "totalP")
-
-  let buttonn = document.createElement("button")
-  buttonn.textContent = "CHECKOUT"
-  buttonn.addEventListener('click',()=>{
-
-    // authentication
-    let arr71 = JSON.parse(localStorage.getItem('userlogin'))||[];
-    if(arr71.length == 0){
-      alert('Please Login First to place order');
-    }
-    else{
-      window.location.replace('cart.html')
-    }
-
-
-  })
-
-
-  buttonn.setAttribute("class", "button")
   let a = document.createElement("a")
   a.textContent = "CHECKOUT"
   a.setAttribute("class", "button")
-  // a.setAttribute("href","cart.html")
+  a.setAttribute("href","cart.html")
   
-  total.append(totalP,buttonn)
+
+  total.append(totalP, a)
 
   priceContainer.append(total)
 
 
-
-
-
-
-  priceContainer.innerHTML = null;
-  let total = document.createElement("div");
-  let totalP = document.createElement("h2");
-  totalP.textContent = `Subtotal‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ${totalPrice}.00`;
-  totalP.setAttribute("class", "totalP");
-
-  let button = document.createElement("button");
-  button.textContent = "CHECKOUT";
-  button.addEventListener("click", () => {
-    // authentication
-    let arr7 = JSON.parse(localStorage.getItem("userlogin")) || [];
-    if (arr7.length == 0) {
-      alert("Please Login First to place order");
-    } else {
-      window.location.replace("cart.html");
-    }
-  });
-  button.setAttribute("class", "button");
-  let a = document.createElement("a");
-  a.textContent = "CHECKOUT";
-  a.setAttribute("class", "button");
-  a.setAttribute("href", "cart.html");
-
-
-  total.append(totalP, a);
-
-  priceContainer.append(total);
 }
 
-//const url = "https://striped-telling-dryer.glitch.me/pizza"
+// function displaydata3() {
+  
+//   priceContainer.innerHTML = null
+//   let total = document.createElement("div")
+//   let totalP = document.createElement("h2")
+//   totalP.textContent = `Subtotal‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ${totalPrice}.00`
+//   totalP.setAttribute("class", "totalP")
+
+//   let buttonn = document.createElement("button")
+//   buttonn.textContent = "CHECKOUT"
+//   buttonn.addEventListener('click',()=>{
+
+//     // authentication
+//     let arr71 = JSON.parse(localStorage.getItem('userlogin'))||[];
+//     if(arr71.length == 0){
+//       alert('Please Login First to place order');
+//     }
+//     else{
+//       window.location.replace('cart.html')
+//     }
+
+
+//   })
+
+
+//   buttonn.setAttribute("class", "button")
+//   let a = document.createElement("a")
+//   a.textContent = "CHECKOUT"
+//   a.setAttribute("class", "button")
+//   // a.setAttribute("href","cart.html")
+  
+//   total.append(totalP,buttonn)
+
+//   priceContainer.append(total)
+
+
+
+
+
+
+
+
+// }
+
+
+
 
 //chandrakala
 // ***********
@@ -460,7 +565,7 @@ login.addEventListener("click", () => {
 
   formDiv.innerHTML = ` <div id="logindiv">
                 <h4>Welcome Back!</h4>
-                <form action="/" method="post" id="loginForm">
+                
                     <table>
                         <tr>
                             <td><label>
@@ -483,7 +588,7 @@ login.addEventListener("click", () => {
                                 </tr>
                                 </table>
                     <button class="button1" class="loginbutton" />Log In</button>
-                </form>
+               
                 <p id="forgot"><a>Forgot_password</a></p>
             </div>
     
@@ -711,6 +816,6 @@ let signupEelement = document.getElementById("signup");
             }
         }
     }
-
-
 // ***************
+
+
